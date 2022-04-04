67ca70eec4a742411545477ef05f16af10a0ffcc
ohnadir <qx.nadir@gmail.com>
2022-04-04 15:57:31 +0600

    hamburger icon add complete

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import CustomLink from '../../CustomLink/CustomLink';
-
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <div className='bg-neutral-700 p-4'>
-            <nav className='flex gap-8 justify-center items-center'>
-                <CustomLink to='/home'>Home</CustomLink>
-                <CustomLink to='/reviews'>Reviews</CustomLink>
-                <CustomLink to='/dashboard'>DashBoard</CustomLink>
-                <CustomLink to='/blogs'>Blogs</CustomLink>
-                <CustomLink to='/about'>About</CustomLink>
-            </nav>
+        <div className='bg-slate-700'>
+
+            <div className=''>
+                <div  className=' md:hidden p-4  text-right'>
+                    <button onClick={()=> setOpen(!open)} className=' text-white '>
+                            { open ? <FontAwesomeIcon icon={faX}/>: <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>}
+                    </button>
+                </div>
+                <nav className={`md:flex p-4 text-center bg-slate-700 w-full gap-8 justify-center absolute md:static duration-500 ease-in ${open ? 'top-10': 'top-[-130px]'}`}>
+                    <CustomLink to='/home'>Home</CustomLink>
+                    <CustomLink to='/reviews'>Reviews</CustomLink>
+                    <CustomLink to='/dashboard'>DashBoard</CustomLink>
+                    <CustomLink to='/blogs'>Blogs</CustomLink>
+                    <CustomLink to='/about'>About</CustomLink>
+                </nav>
+                
+            </div>
+            
         </div>
     );
 };
